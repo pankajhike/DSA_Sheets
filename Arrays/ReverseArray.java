4876e16165e1fbcf525e8d4698b16a19cf382514
PANKAJ KUMAR MUNDA <50673726+pankajhike@users.noreply.github.com>
2023-09-02 00:33:42 +0530

    ReverseArray

---
+++ Arrays/ReverseArray.java
@@ -0,0 +1,21 @@
+package DSA_Sheets.Arrays;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        int start = 0;
+        int end=arr.length-1;
+
+        while (start<end){
+            int temp = arr[end];
+            arr[end] = arr[start];
+            arr[start]=temp;
+
+            start++;
+            end--;
+        }
+    for (int i=0; i<arr.length; i++){
+        System.out.print(arr[i]+" ");
+    }
+    }
+}
